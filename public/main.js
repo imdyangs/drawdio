a7ec154edd1927d79033e4acb99c4711edd107ef
imdyangs <iamdyangster@gmail.com>
2017-01-22 04:01:28 -0500

    back button

--- public/main.js
+++ public/main.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
   console.log("hiding!");
   $('#songDetail').hide();
   $('#back-icon').fadeOut();
-    
+
 });
 
 // Spotify api call
@@ -52,35 +52,37 @@ function getDataURL(){
 //show canvas, draw button
 function showCanvas(){
 
-    function downsize(){
-      TweenMax.to('.discAnimate', 1.5, {scale: 0.3})
-    }
+  function downsize(){
+    TweenMax.to('.discAnimate', 1.5, {scale: 0.3})
+  }
 
-    //hide textbar
-    TweenMax.to('#introbar', 1.5, { ease: Power2.easeInOut, x: -1000});
+  //hide textbar
+  TweenMax.to('#introbar', 1.5, { ease: Power2.easeInOut, x: -1000});
 
-    //hide vinyl
-    TweenMax.to('.discAnimate', 1.5, { ease: Power2.easeInOut, rotation: 60, x: -308, onComplete:downsize});
+  //hide vinyl
+  TweenMax.to('.discAnimate', 1.5, { ease: Power2.easeInOut, rotation: 60, x: -308, onComplete:downsize});
 
-    //show textbar
-    TweenMax.from(document.getElementById('helptext'), 1.5, { ease: Power2.easeInOut, x: 750});
+  //show textbar
+  TweenMax.fromTo(document.getElementById('helptext'), 1.5, { ease: Power2.easeInOut, x: 750}, {x:0});
 
 
-    //fade in pad
-    TweenMax.from(document.getElementById('aww-wrapper'), 1, { ease: SlowMo.ease.config(0.1, 0.1, false), opacity:0, delay: 0.7});
+  //fade in pad
+  //TweenMax.from(document.getElementById('aww-wrapper'), 1, { ease: SlowMo.ease.config(0.1, 0.1, false), opacity:0, delay: 0.7});
+  TweenMax.fromTo(document.getElementById('aww-wrapper'), 1.8, {opacity: 0, delay:0.4}, {ease: Power2.easeInOut, opacity:1});
+  //fade in returnButton
+  TweenMax.to('.btnBox', 1.5, { ease: Power2.easeInOut, x: 450, delay: 1})
 
-    //fade in returnButton
-    TweenMax.to('.btnBox', 1.5, { ease: Power2.easeInOut, x: 450, delay: 1})
+  /* add drawPad */
+  $('#aww-wrapper').show();
+  $('#helptext').show();
 
-    /* add drawPad */
-    $('#aww-wrapper').show();
-    $('#helptext').show();
-    
-    /* show backButton */
-    $('#back-icon').fadeIn();
-    
-    /* keep track of current page */
-    curPage = 'canvas';
+
+  /* show backButton */
+  $('#back-icon').fadeIn();
+  TweenMax.to(document.getElementById('back-icon'), 1.2, {ease: Power2.easeInOut, x:0});
+  /* keep track of current page */
+  curPage = 'canvas';
+
 }
 
 //show song details, go button
@@ -88,18 +90,18 @@ function showCanvas(){
 
 
 function showSongDetail(){
-    var currentSpotifyURI = "spotify:artist:44gRHbEm4Uqa0ykW0rDTNk";
-    //hide textbarAlbums
-    TweenMax.to(document.getElementById('helptext'), 1.5, { ease: Power2.easeInOut, x: 2000});
+  var currentSpotifyURI = "spotify:artist:44gRHbEm4Uqa0ykW0rDTNk";
+  //hide textbarAlbums
+  TweenMax.to(document.getElementById('helptext'), 1.5, { ease: Power2.easeInOut, x: 2000});
 
-    //show vinyl
-    TweenMax.to('.discAnimate', 0, {scale: 1});
-    TweenMax.to('.discAnimate', 2.4, { ease: Power2.easeInOut, rotation: 20, x: -170 , scale: 1, delay: 0.8});
-    TweenMax.to('.discAnimate', 1.6, { ease: Power2.easeInOut, rotation: 60, x: -380, delay: 2.9 });
-    canReveal = true;
-    
-    
-    curPage = 'song';
+  //show vinyl
+  TweenMax.to('.discAnimate', 0, {scale: 1});
+  TweenMax.to('.discAnimate', 2.4, { ease: Power2.easeInOut, rotation: 20, x: -170 , scale: 1, delay: 0.8});
+  TweenMax.to('.discAnimate', 1.6, { ease: Power2.easeInOut, rotation: 60, x: -380, delay: 2.9 });
+  canReveal = true;
+  TweenMax.to(document.getElementById('aww-wrapper'), 2, { ease: Power2.easeInOut, opacity: 0});
+
+  curPage = 'song';
 }
 
 
@@ -114,7 +116,7 @@ function reveal(res) {
     //take the current spotify URI, extract the image link, then overlay image ontop of disc
 
     //hide pad
-    TweenMax.to(document.getElementById('aww-wrapper'), 2, { ease: Power2.easeInOut, opacity: 0});
+  //  TweenMax.to(document.getElementById('aww-wrapper'), 2, { ease: Power2.easeInOut, opacity: 0});
     TweenMax.to(document.getElementById('cover'), 3, { ease: Power2.easeInOut, x: -3300, delay: 9.3});
 
     //hide return
@@ -130,7 +132,7 @@ function lookForSong(){
     //$('#aww-wrapper').hide();
     //$('#cover').hide();
     //$('#helptext').hide();
-    $('#songDetail').show();
+  //  $('#songDetail').show();
     $('#disc').addClass('clip-circle');
 
     // roll vinyl over
@@ -245,41 +247,52 @@ function prevSong(){
 
 /* Handle hide/show element of page transition */
 function backToCanvas(){
-    
-    console.log('you invoked backToCanvas!');
-    
-    /* hide song detail element */
-    $('#songDetail').hide();
-    $('#disc').attr('src', 'image/disc.png').removeClass('clip-circle');
-    
-    /* show canvas */
-    $('#helptext').show();
-    $('#cover').show();
-    $('#aww-wrapper').show();
-    
-    /* update current page location */
-    curPage = 'canvas';
+
+  console.log('you invoked backToCanvas!');
+
+  /* hide song detail element */
+//  $('#songDetail').hide();
+  TweenMax.fromTo(document.getElementById('songDetail'), 2.2, {x:0} ,{ ease: Power2.easeOut, x: -800, delay: 9.8});
+//  $('#disc').attr('src', 'image/disc.png').removeClass('clip-circle');
+  TweenMax.to('.discAnimate', 1.5, { ease: Power2.easeInOut, rotation: 60, x: -308});
+
+  /* show stuff */
+  //$('#helptext').show();
+  TweenMax.fromTo(document.getElementById('helptext'), 1.5, { ease: Power2.easeInOut, x: 750}, {x:0});
+
+  //$('#cover').show();
+  TweenMax.fromTo(document.getElementById('cover'), 3, { ease: Power2.easeInOut, x: -3300, delay: 1.3}, {x:70});
+
+//    $('#aww-wrapper').show();
+  TweenMax.fromTo(document.getElementById('aww-wrapper'), 1.8, {opacity: 0, delay:0.4}, {ease: Power2.easeInOut, opacity:1});
+
+  /* update current page location */
+  curPage = 'canvas';
 }
 
 function backToHome(){
-    
-    console.log('you invoked backToHome!');
-    
-    /* hide canvas elements */
-    $('#helptext').hide();
-    $('#aww-wrapper').hide();
-    
-    /* show homepage elements */
-    $('#introbar').show();
-    
-    /* missing line: vinyl row over */
-    $('#disc').show();
-    
-    /* hide back icon */
-    $('#back-icon').fadeOut();
-    
-    /* update current page location */
-    curPage = 'home';
+
+  console.log('you invoked backToHome!');
+
+  /* hide canvas elements */
+//  $('#helptext').hide();
+  TweenMax.to(document.getElementById('helptext'), 1.8, {ease: Power2.easeInOut, x: 800});
+  //$('#aww-wrapper').hide();
+
+  TweenMax.fromTo(document.getElementById('aww-wrapper'), 1.8, {opacity: 1}, {ease: Power2.easeInOut, opacity:0});
+  /* show homepage elements */
+//  $('#introbar').show();
+  TweenMax.to(document.getElementById('introbar'), 1.8, {ease: Power2.easeInOut, x:0});
+
+  /* missing line: vinyl row over */
+  TweenMax.to(document.getElementById('disc'), 0.3, {scale: 1});
+  TweenMax.to(document.getElementById('disc'), 1.8, {ease: Power2.easeInOut, x:0, rotation: 0});
+
+  /* hide back icon */
+//  $('#back-icon').fadeOut();
+  TweenMax.to(document.getElementById('back-icon'), 1.2, {ease: Power2.easeInOut, x:-300});
+  /* update current page location */
+  curPage = 'home';
 }
 
 /* Handle page transition, whenever back button is clicked */
@@ -314,4 +327,3 @@ $('#back-icon').hover(function(){
 }, function(){
     $('#back-icon').attr('src', './image/buttons/back.png');
 });
-
